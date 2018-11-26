9f0548262841a6e783bafe49f39452053c7804c8
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-11-26 09:28:18 +0100

    prepare standard streams init in study
    -and refactor standard streams to reoccuring

--- bix/evaluation/study.py
+++ bix/evaluation/study.py
@@ -1,7 +1,5 @@
-import math
-import copy
-import sys
 import os
+import datetime
 
 from bix.utils.reoccuringdriftstream import ReoccuringDriftStream 
 from skmultiflow.data.concept_drift_stream import ConceptDriftStream
@@ -16,9 +14,10 @@ from skmultiflow.data.sea_generator import SEAGenerator
 from skmultiflow.data.hyper_plane_generator import HyperplaneGenerator
 # No Concept Drift Generators
 from skmultiflow.data.random_rbf_generator import RandomRBFGenerator
-import datetime
+
+
 class Study():
-## TODO: List of string with stream names for individual studies
+    # TODO: List of string with stream names for individual studies
     def __init__(self,streams=None,path="/"):
         if streams == None:
                 self.streams = self.init_standard_streams()
@@ -34,31 +33,35 @@ class Study():
             if not os.path.exists(self.path):
                 os.mkdir(self.path)
         except Exception as e: raise FileNotFoundError("Error while creating Directory!")
-
+    
     def init_standard_streams(self):
-        s1 = SineGenerator(classification_function=0,balance_classes=False,random_state=112)
-        s2 = SineGenerator(classification_function=1,balance_classes=False, random_state=112)
-        ra_sine = ReoccuringDriftStream(stream=s1, drift_stream=s2,alpha=90.0,position=2000,width=1)
-        rg_sine = ReoccuringDriftStream(stream=s1, drift_stream=s2,alpha=90.0,position=2000,width=1000)
+        """Initializes standard data streams"""
 
-        stagger1 = STAGGERGenerator(classification_function=0,balance_classes=False,random_state=112)
-        stagger2 = STAGGERGenerator(classification_function=1,balance_classes=False,random_state=112)
-        ra_stagger = ReoccuringDriftStream(stream=stagger1, drift_stream=stagger2, random_state=112,alpha=90.0,position=2000,width=1)
-        rg_stagger = ReoccuringDriftStream(stream=stagger1, drift_stream=stagger2, random_state=112,alpha=90.0,position=2000,width=1000)
+    def init_reoccuring_streams(self):
+        """Initializes reoccuring streams: abrupt and gradual"""
+        s1 = SineGenerator(classification_function=0, balance_classes=False, random_state=112)
+        s2 = SineGenerator(classification_function=1, balance_classes=False, random_state=112)
+        ra_sine = ReoccuringDriftStream(stream=s1, drift_stream=s2, alpha=90.0, position=2000, width=1)
+        rg_sine = ReoccuringDriftStream(stream=s1, drift_stream=s2, alpha=90.0, position=2000, width=1000)
 
-        sea1 = SEAGenerator(classification_function=0,balance_classes=False,random_state=112)
-        sea2 = SEAGenerator(classification_function=1,balance_classes=False,random_state=112)
-        ra_sea = ReoccuringDriftStream(stream=sea1, drift_stream=sea2, random_state=112,alpha=90.0,position=2000,width=1)
-        rg_sea = ReoccuringDriftStream(stream=sea1, drift_stream=sea2, random_state=112,alpha=90.0,position=2000,width=1000)
+        stagger1 = STAGGERGenerator(classification_function=0, balance_classes=False, random_state=112)
+        stagger2 = STAGGERGenerator(classification_function=1, balance_classes=False, random_state=112)
+        ra_stagger = ReoccuringDriftStream(stream=stagger1, drift_stream=stagger2, random_state=112, alpha=90.0,position=2000,width=1)
+        rg_stagger = ReoccuringDriftStream(stream=stagger1, drift_stream=stagger2, random_state=112, alpha=90.0,position=2000,width=1000)
 
-        mixed1 = MIXEDGenerator(classification_function=0,random_state=112,balance_classes=False)
-        mixed2 = MIXEDGenerator(classification_function=1,random_state=112,balance_classes=False)
-        ra_mixed = ReoccuringDriftStream(stream=mixed1, drift_stream=mixed2, random_state=112,alpha=90.0,position=2000,width=1)
-        rg_mixed = ReoccuringDriftStream(stream=mixed1, drift_stream=mixed2, random_state=112,alpha=90.0,position=2000,width=1000)
+        sea1 = SEAGenerator(classification_function=0, balance_classes=False, random_state=112)
+        sea2 = SEAGenerator(classification_function=1, balance_classes=False, random_state=112)
+        ra_sea = ReoccuringDriftStream(stream=sea1, drift_stream=sea2, random_state=112, alpha=90.0, position=2000,width=1)
+        rg_sea = ReoccuringDriftStream(stream=sea1, drift_stream=sea2, random_state=112, alpha=90.0, position=2000,width=1000)
+
+        mixed1 = MIXEDGenerator(classification_function=0, random_state=112, balance_classes=False)
+        mixed2 = MIXEDGenerator(classification_function=1, random_state=112, balance_classes=False)
+        ra_mixed = ReoccuringDriftStream(stream=mixed1, drift_stream=mixed2, random_state=112, alpha=90.0, position=2000,width=1)
+        rg_mixed = ReoccuringDriftStream(stream=mixed1, drift_stream=mixed2, random_state=112, alpha=90.0, position=2000,width=1000)
 
         inc = HyperplaneGenerator(random_state=112)
         
-        return [ra_sine,rg_sine,ra_stagger,rg_stagger,ra_sea,rg_sea,ra_mixed,rg_mixed,inc]
+        return [ra_sine, rg_sine, ra_stagger, rg_stagger, ra_sea, rg_sea, ra_mixed, rg_mixed, inc]
 
 
     def init_real_world(self):
@@ -73,7 +76,7 @@ class Study():
             gmsc = FileStream('../datasets/gmsc.csv')
            # airlines = FileStream('datasets/airlines.csv') label failure
             moving_squares = FileStream('../datasets/moving_squares.csv')
-            return [elec,weather,gmsc,moving_squares]
+            return [elec, weather, gmsc, moving_squares]
         except Exception as e: 
             raise FileNotFoundError("Real-world datasets can't loaded! Check directory Datasets/")
             return []
