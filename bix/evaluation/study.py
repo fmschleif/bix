84e6a6e20dd7f457c9f1aeb209a28c241a85024c
Moritz Heusinger <moritz.heusinger@gmail.com>
2018-11-26 12:58:21 +0100

    added more standard streams
    -and fixed import

--- bix/evaluation/study.py
+++ bix/evaluation/study.py
@@ -1,7 +1,7 @@
 import os
 import datetime
 
-from bix.utils.reoccuringdriftstream import ReoccuringDriftStream 
+from bix.utils.ReoccuringDriftStream import ReoccuringDriftStream 
 from skmultiflow.data.concept_drift_stream import ConceptDriftStream
 from skmultiflow.data.file_stream import FileStream
 #Abrupt Concept Drift Generators
@@ -9,17 +9,19 @@ from skmultiflow.data.stagger_generator import STAGGERGenerator
 from skmultiflow.data.mixed_generator import MIXEDGenerator
 from skmultiflow.data.sea_generator import SEAGenerator
 from skmultiflow.data.sine_generator import SineGenerator
-from skmultiflow.data.sea_generator import SEAGenerator
 from skmultiflow.data.agrawal_generator import AGRAWALGenerator
+from skmultiflow.data.random_tree_generator import RandomTreeGenerator
 # Incremental Concept Drift Generators
 from skmultiflow.data.hyper_plane_generator import HyperplaneGenerator
+from skmultiflow.data.led_generator_drift import LEDGeneratorDrift
+from skmultiflow.data.random_rbf_generator_drift import RandomRBFGeneratorDrift
 # No Concept Drift Generators
 from skmultiflow.data.random_rbf_generator import RandomRBFGenerator
-from skmultiflow.data.hyper_plane_generator import HyperplaneGenerator
 
 class Study():
+    """Study"""
     # TODO: List of string with stream names for individual studies
-    def __init__(self,streams=None,path="/"):
+    def __init__(self, streams=None, path="/"):
         if streams == None:
                 self.streams = self.init_standard_streams()
         else:
@@ -36,7 +38,7 @@ class Study():
         except Exception as e: raise FileNotFoundError("Error while creating Directory!")
     
     def init_standard_streams(self):
-        """Initializes standard data streams"""
+        """Initialize standard data streams"""
         agrawal_a = ConceptDriftStream(stream=AGRAWALGenerator(random_state=112, perturbation=0.1), 
                             drift_stream=AGRAWALGenerator(random_state=112, 
                                                           classification_function=2, perturbation=0.1),
@@ -52,13 +54,44 @@ class Study():
                             width=50000)
                     
         hyper = HyperplaneGenerator(mag_change=0.001, noise_percentage=0.1)
-                        
-                            
-        return [agrawal_a, agrawal_g, hyper]
         
-
+        led_a = ConceptDriftStream(stream=LEDGeneratorDrift(has_noise=False, noise_percentage=0.0, n_drift_features=3),
+                            drift_stream=LEDGeneratorDrift(has_noise=False, noise_percentage=0.0, n_drift_features=7),
+                            random_state=None,
+                            alpha=90.0,
+                            position=250000)
+        
+        led_g = ConceptDriftStream(stream=LEDGeneratorDrift(has_noise=False, noise_percentage=0.0, n_drift_features=3),
+                            drift_stream=LEDGeneratorDrift(has_noise=False, noise_percentage=0.0, n_drift_features=7),
+                            random_state=None,
+                            position=250000,
+                            width=50000)
+        
+        rand_tree = RandomTreeGenerator()
+        
+        rbf_if = RandomRBFGeneratorDrift(change_speed=0.001)
+        
+        rbf_im = RandomRBFGeneratorDrift(change_speed=0.0001)
+        
+        sea_a = ConceptDriftStream(stream=SEAGenerator(random_state=112, noise_percentage=0.1), 
+                            drift_stream=SEAGenerator(random_state=112, 
+                                                          classification_function=2, noise_percentage=0.1),
+                            alpha=90.0,
+                            random_state=None,
+                            position=250000,
+                            width=1)  
+                            
+        sea_g = ConceptDriftStream(stream=SEAGenerator(random_state=112, noise_percentage=0.1), 
+                            drift_stream=SEAGenerator(random_state=112, 
+                                                          classification_function=1, noise_percentage=0.1),
+                            random_state=None,
+                            position=250000,
+                            width=50000)
+                            
+        return [agrawal_a, agrawal_g, hyper, led_a, led_g, rand_tree, rbf_if, rbf_im, sea_a, sea_g]
+        
     def init_reoccuring_streams(self):
-        """Initializes reoccuring streams: abrupt and gradual"""
+        """Initialize reoccuring data streams: abrupt and gradual"""
         s1 = SineGenerator(classification_function=0, balance_classes=False, random_state=112)
         s2 = SineGenerator(classification_function=1, balance_classes=False, random_state=112)
         ra_sine = ReoccuringDriftStream(stream=s1, drift_stream=s2, alpha=90.0, position=2000, width=1)
@@ -83,8 +116,8 @@ class Study():
         
         return [ra_sine, rg_sine, ra_stagger, rg_stagger, ra_sea, rg_sea, ra_mixed, rg_mixed, inc]
 
-
     def init_real_world(self):
+        """Initialize real world data streams, will be loaded from file"""
         if not os.path.exists("datasets/"):
             raise FileNotFoundError("Folder for data cannot be found! Should be datasets/")
          
