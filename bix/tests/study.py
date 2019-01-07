c02b35cf965077677d640cee3ecf4d05eb47518d
Christoph <christophraab@outlook.de>
2019-01-07 16:25:22 +0100

    study test as unit test

--- bix/tests/study.py
+++ bix/tests/study.py
@@ -9,38 +9,38 @@ from skmultiflow.bayes import NaiveBayes
 from skmultiflow.meta.adaptive_random_forests import AdaptiveRandomForest
 from skmultiflow.trees.hoeffding_adaptive_tree import HAT
 import os
-def parameter_grid_search_test():
-    grid = {"sigma": np.arange(2,3,2), "prototypes_per_class": np.arange(2,3,2)}
-    clf = RRSLVQ()
-    gs = GridSearch(clf=clf,grid=grid,max_samples=1000)
-    gs.search()
-    gs.save_summary()
+import unittest
+class TestStudy(unittest.TestCase):
 
-def cross_validation_real_world_test():
-    clfs = [RRSLVQ(prototypes_per_class=2), HAT()]
-    
-    cv = CrossValidation(clfs=clfs,max_samples=500,test_size=1)
-    cv.streams = cv.init_real_world()
-    cv.test()
-    cv.save_summary()
+    def test_parameter_grid_search_test(self):
+        grid = {"sigma": np.arange(2,3,2), "prototypes_per_class": np.arange(2,3,2)}
+        clf = RRSLVQ()
+        gs = GridSearch(clf=clf,grid=grid,max_samples=1000)
+        gs.search()
+        gs.save_summary()
 
-def cross_validation_standard_test():
-    clfs = [RRSLVQ(prototypes_per_class=2), HAT()]
-    
-    cv = CrossValidation(clfs=clfs,max_samples=500,test_size=1)
-    cv.test()
-    cv.save_summary()
+    def test_cross_validation_real_world_test(self):
+        clfs = [RRSLVQ(prototypes_per_class=2), HAT()]
+        
+        cv = CrossValidation(clfs=clfs,max_samples=500,test_size=1)
+        cv.streams = cv.init_real_world()
+        cv.test()
+        cv.save_summary()
 
-def cross_validation_reoccuring_test():
-    clfs = [RRSLVQ(prototypes_per_class=2), HAT()]
-    
-    cv = CrossValidation(clfs=clfs,max_samples=500,test_size=1)
-    cv.streams= cv.init_reoccuring_streams()
-    cv.test()
-    cv.save_summary()
+    def test_cross_validation_standard_test(self):
+        clfs = [RRSLVQ(prototypes_per_class=2), HAT()]
+        
+        cv = CrossValidation(clfs=clfs,max_samples=500,test_size=1)
+        cv.test()
+        cv.save_summary()
+
+    def test_cross_validation_reoccuring_test(self):
+        clfs = [RRSLVQ(prototypes_per_class=2), HAT()]
+        
+        cv = CrossValidation(clfs=clfs,max_samples=500,test_size=1)
+        cv.streams= cv.init_reoccuring_streams()
+        cv.test()
+        cv.save_summary()
 
 if __name__ == "__main__":  
-  # parameter_grid_search_test()
-#  cross_validation_standard_test()
- #cross_validation_real_world_test()
- cross_validation_reoccuring_test()
+    unittest.main()
