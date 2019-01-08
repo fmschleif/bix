5172fa329c4cb8c256c68b392d57e9c5e510ce8e
Moritz Heusinger <moritz.heusinger@gmail.com>
2019-01-08 15:48:57 +0100

    fix

--- bix/classifiers/adaptive_rslvq.py
+++ bix/classifiers/adaptive_rslvq.py
@@ -232,31 +232,6 @@ class RSLVQ(ClassifierMixin, StreamModel, BaseEstimator):
                     # Update Prototype
                     self.w_[j] += (self.learning_rate / ((self.squared_mean_gradient[j] + \
                            self.epsilon) ** 0.5)) * gradient
-
-
-                for j in range(prototypes.shape[0]):
-                    d = (xi - prototypes[j])
-                    
-                    if self.c_w_[j] == c_xi:
-                        gradient = (self._p(j, xi, prototypes=self.w_, y=c_xi) -
-                                     self._p(j, xi, prototypes=self.w_)) * d
-                    else:
-                        gradient = - self._p(j, xi, prototypes=self.w_) * d
-                        
-                     # Accumulate gradient
-                    self.squared_mean_gradient[j] = self.decay_rate * self.squared_mean_gradient[j] + \
-                                (1 - self.decay_rate) * gradient ** 2
-                    
-                    # Compute update/step
-                    step = ((self.squared_mean_step[j] + self.epsilon) / \
-                              (self.squared_mean_gradient[j] + self.epsilon)) ** 0.5 * gradient
-                              
-                    # Accumulate updates
-                    self.squared_mean_step[j] = self.decay_rate * self.squared_mean_step[j] + \
-                    (1 - self.decay_rate) * step ** 2
-                    
-                    # Attract/Distract prototype to/from data point
-                    self.w_[j] += step
      
     def _costf(self, x, w, **kwargs):
         d = (x - w)[np.newaxis].T 
