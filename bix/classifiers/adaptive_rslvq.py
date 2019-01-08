5a41f8bedfcbec87a9c06e27359fca9c514d4cf9
Moritz Heusinger <moritz.heusinger@gmail.com>
2019-01-08 16:01:23 +0100

    restore lines

--- bix/classifiers/adaptive_rslvq.py
+++ bix/classifiers/adaptive_rslvq.py
@@ -207,6 +207,30 @@ class RSLVQ(ClassifierMixin, StreamModel, BaseEstimator):
                         
                 k += self.batch_size
                 
+                for j in range(prototypes.shape[0]):
+                    d = (xi - prototypes[j])
+                    
+                    if self.c_w_[j] == c_xi:
+                        gradient = (self._p(j, xi, prototypes=self.w_, y=c_xi) -
+                                     self._p(j, xi, prototypes=self.w_)) * d
+                    else:
+                        gradient = - self._p(j, xi, prototypes=self.w_) * d
+                        
+                     # Accumulate gradient
+                    self.squared_mean_gradient[j] = self.decay_rate * self.squared_mean_gradient[j] + \
+                                (1 - self.decay_rate) * gradient ** 2
+                    
+                    # Compute update/step
+                    step = ((self.squared_mean_step[j] + self.epsilon) / \
+                              (self.squared_mean_gradient[j] + self.epsilon)) ** 0.5 * gradient
+                              
+                    # Accumulate updates
+                    self.squared_mean_step[j] = self.decay_rate * self.squared_mean_step[j] + \
+                    (1 - self.decay_rate) * step ** 2
+                    
+                    # Attract/Distract prototype to/from data point
+                    self.w_[j] += step
+                
         elif(self.gradient_descent=='RMSprop'):
             """Implementation of RMSprop"""
             n_data, n_dim = X.shape
