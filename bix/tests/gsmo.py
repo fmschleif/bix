a0ad98f084ebcde760d04afa85b533d2e27f3b44
simonheilig <simon.heilig@web.de>
2020-05-06 14:45:39 +0200

    G-SMO solver
    assert Cx=d

--- bix/tests/gsmo.py
+++ bix/tests/gsmo.py
@@ -2,19 +2,21 @@ import unittest
 import numpy as np
 from bix.utils.gsmo_solver import GSMO
 
+
 class TESTGSMO(unittest.TestCase):
     def test_init_x(self):
         # Arrange
         C = np.array([-1, 1, -1])
-        d = 0
-        x_solution = [0.75, 1.5, 0.75]
+        d = 3
 
         # Act
-        gsmo_solver = GSMO(A=np.zeros((3,3)),b=np.zeros((3,1)),C=C,d=d,r=0,R=5)
+        gsmo_solver = GSMO(A=np.zeros((3, 3)), b=np.zeros((3, 1)), C=C, d=d, r=0, R=5)
 
         # Assert
-        for x1, x2 in tuple(zip(x_solution, gsmo_solver.x)):
-            self.assertAlmostEqual(x1, x2)
+        # Cx = d
+        result = C.dot(gsmo_solver.x)
+        self.assertAlmostEqual(d, result)
+
 
 if __name__ == '__main__':
     unittest.main()
