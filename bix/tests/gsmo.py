8747db3900fb1f49b0fdf622a6b2814b5a3d1b2e
simonheilig <simon.heilig@web.de>
2020-05-06 14:34:42 +0200

    G-SMO solver
    clean up test

--- bix/tests/gsmo.py
+++ bix/tests/gsmo.py
@@ -6,13 +6,6 @@ from bix.utils.gsmo_solver import GSMO
 class TESTGSMO(unittest.TestCase):
     def test_init_x(self):
         # Arrange
-        def f(x, C, d):
-            y = np.dot(C, x) - d
-            return np.dot(y, y)
-
-        bounds = tuple([(0, 5) for _ in range(3)])
-        C = np.array([-1, 1, -1])
-        d = 0
         x_solution = [0.75, 1.5, 0.75]
 
         # Act
